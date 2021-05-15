1a96a856723a730a3d9b47128d27d189e78247f4
Mahesh Chavan <maheshchavan0806@gmail.com>
2021-05-15 16:33:40 -0400

    Add project files.

---
+++ RestApiCRUD/EmployeeData/IEmployeeData.cs
@@ -0,0 +1,19 @@
+﻿using RestApiCRUD.Models;
+using System;
+using System.Collections.Generic;
+
+namespace RestApiCRUD.EmployeeData
+{
+    public interface IEmployeeData
+    {
+        List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid id);
+
+        Employee AddEmployee(Employee employee);
+
+        void DeleteEmployee(Employee employee);
+
+        Employee EditEmployee(Employee employee);
+    }
+}
---
+++ RestApiCRUD/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestApiCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public Guid Id { get; set; }
+        
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name can only 50 characters long")]
+        public string Name { get; set; }
+    }
+}
